429b95998b8ff412afe255c4d93d07423ee11deb
Srijan-18 <srijan.mishrasir@gmail.com>
2020-08-14 04:05:05 +0530

    REFACTORED: Improvement made in code hygiene.

--- random_dice.cpp
+++ random_dice.cpp
@@ -7,6 +7,5 @@ using namespace std;
 int main() {
 	srand(time(0));
 	  int randomNumber =(rand() % 6) + 1;
-	  //cout << "Seed -->" << time(0) << "\n";
 	  cout << "Dice face --> " << randomNumber << endl;
 }
